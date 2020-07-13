1f6be419007efe859fdfad4bf950d0a3f42c3062
AlbertZJ <1353296585@qq.com>
2020-07-13 14:51:20 +0800

    init project

--- src/main/java/com/zhuishu/tea/controller/TeaController.java
+++ src/main/java/com/zhuishu/tea/controller/TeaController.java
@@ -1,6 +1,7 @@
 package com.zhuishu.tea.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,38 +10,42 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/tea")
 public class TeaController {
 
+    @ApiOperation(value = "添加tea的接口", notes = "添加一个tea的接口")
+    @ApiImplicitParam(name = "teaRoutForm", value = "新增tea的form表单", required = true, dataType = "Tea")
     @PostMapping
-    @ApiOperation("添加tea的接口")
     public String add(){
         return "add tea";
     }
 
+    @ApiOperation(value = "删除tea的接口", notes = "根据tea的id来指定删除对象")
+    @ApiImplicitParam(paramType = "path", name = "id", value = "茶的ID", required = true, dataType = "string")
     @DeleteMapping("/{id}")
-    @ApiOperation("删除tea的接口")
     public String delete(@PathVariable String id){
         return "delete tea"+id;
     }
 
-    @PutMapping("/{id}")
-    @ApiOperation("修改tea的接口")
+    @ApiOperation(value = "修改tea的接口")
+    @PutMapping(value = "/{id}")
     public String put(@PathVariable String id){
         return "put tea"+id;
     }
 
-    @GetMapping("/{id}")
-    @ApiOperation("查询tea的接口")
+    @ApiOperation(value = "查询tea的接口")
+    @ApiImplicitParam(paramType = "path", name = "id", value = "tea的ID", required = true, dataType = "string")
+    @GetMapping(value = "/{id}")
     public String get(@PathVariable String id){
         return "get tea"+id;
     }
 
-    @PostMapping("/conditions")
     @ApiOperation("搜索tea的接口")
+    @ApiImplicitParam(name = "teaRoutQueryForm", value = "tea查询参数", required = true, dataType = "TeaRouteQueryForm")
+    @PostMapping(value = "/conditions")
     public String search(){
         return "search tea";
     }
 
+    @ApiOperation(value = "查询所有tea的接口")
     @GetMapping("/all")
-    @ApiOperation("查询所有tea的接口")
     String post(){
         return  "欢迎来到tea微服务模块tea!!!";
     }
